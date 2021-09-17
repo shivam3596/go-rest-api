561d48248d548ffbe8b2ee76a35e29cb63824c88
shivam3596 <shivamchauhan3596@gmail.com>
2021-09-17 18:26:35 +0530

    added go post api, redis connection, rabbitmq

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/shivam3596/go-rest-api
+
+go 1.16
+
+require (
+	github.com/go-redis/redis v6.15.9+incompatible // indirect
+	github.com/go-redis/redis/v8 v8.11.3 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/streadway/amqp v1.0.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/go-redis/redis"
+	"github.com/gorilla/mux"
+	"github.com/streadway/amqp"
+)
+
+type Person struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
+	City string `json:"city"`
+}
+
+var redisClient *redis.Client
+var QUEUE_NAME = "hello"
+var REDIS_HOST = "redis-13910.c264.ap-south-1-1.ec2.cloud.redislabs.com:13910"
+var REDIS_PASSWORD = "6XkvmDVxtZz2ChMsKRLLtp4ZYrW3XpwQ"
+var REDIS_DB = 0
+var AMPQ_HOST = "amqps://umgtghjv:ilO7cGhzGKowX5uKUFRFC7CLy8G1G3nQ@puffin.rmq2.cloudamqp.com/umgtghjv"
+
+/*
+	Created redis connection
+	Returns redisClient
+*/
+func InitRedis() *redis.Client {
+	rClient := redis.NewClient(&redis.Options{
+		Addr:     REDIS_HOST,
+		Password: REDIS_PASSWORD,
+		DB:       REDIS_DB,
+	})
+	pong, err := rClient.Ping().Result()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(pong)
+	return rClient
+}
+
+/*
+	Get request body and serializes it
+	Set req body id as redis key along with body as value
+*/
+func createPerson(res http.ResponseWriter, req *http.Request) {
+	var newPerson Person
+
+	decoder := json.NewDecoder(req.Body)
+	err := decoder.Decode(&newPerson)
+	failOnError(err, "Decode person failed")
+
+	serializedPerson, _ := json.Marshal(newPerson)
+
+	if newPerson.Id == "" || newPerson.Name == "" || newPerson.City == "" {
+		http.Error(res, "Incorrect Body", http.StatusBadRequest)
+		return
+	}
+
+	err = redisClient.Set(newPerson.Id, string(serializedPerson), 0).Err()
+	failOnError(err, "Redis set failed")
+
+	sendMessageInQueue(newPerson.Id)
+	res.WriteHeader(http.StatusCreated)
+	json.NewEncoder(res).Encode("Message queued")
+}
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func sendMessageInQueue(body string) {
+	//Rabbitmq connection
+	conn, err := amqp.Dial(AMPQ_HOST)
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	//Create channel
+	channel, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer channel.Close()
+
+	//Declare queue
+	queue, err := channel.QueueDeclare(
+		QUEUE_NAME, // name
+		false,      // durable
+		false,      // delete when unused
+		false,      // exclusive
+		false,      // no-wait
+		nil,        // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	//Publish message
+	err = channel.Publish(
+		"",         // exchange
+		queue.Name, // routing key
+		false,      // mandatory
+		false,      // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	failOnError(err, "Failed to publish a message")
+}
+
+func main() {
+	redisClient = InitRedis()
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/create-person", createPerson).Methods("POST")
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
